eaf1ac8e285a6ad1aef758a4d273c5bcc53e2c32
hjlee9182 <zxzxs9182@gmail.com>
2020-08-06 15:50:14 +0900

    Solved problem using backtracking

---
+++ baekjoon/baekjoon_15666.py
@@ -0,0 +1,31 @@
+import sys
+read = lambda :sys.stdin.readline().strip()
+from collections import deque
+import copy
+from collections import defaultdict
+import itertools
+import math
+
+def dfs(li,num,lis):
+    if num==m:
+        check = int(''.join(map(str,li.split())).strip())
+        if d[check]==0:
+            print(li)
+            d[check] = 1
+        return
+
+    for idx,i in enumerate(lis):
+        dfs(li+' '+str(i),num+1,lis[idx:])
+
+
+n,m = map(int,read().split())
+mat = list(map(int,read().split()))
+mat.sort()
+d = defaultdict(int)
+#
+# for idx,value in enumerate(mat):
+#     dfs(str(value),1,mat[idx:])
+#
+
+for li in sorted(list(set(list(itertools.combinations_with_replacement(mat,m))))):
+    print(*li)
